d79fc691719bfa5f727451ea971a9531fff0d0d8
nguyentien1604 <nguyentiencd89@gmail.com>
2023-11-08 20:06:18 +0700

    20231108_ntien_finish

--- 3. FrontEnd/src/Pages/ProductDetail.jsx
+++ 3. FrontEnd/src/Pages/ProductDetail.jsx
@@ -14,7 +14,7 @@ import { storeRedux } from "../Redux/StoreRedux/Store";
 import { Link, useParams } from "react-router-dom";
 import { actionProductByIDAPI } from "../Redux/Action/ProductAction";
 import axios from "axios";
-import { Rate } from "antd";
+import { Rate,message } from "antd";
 import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
 import { FaRegSmileWink, FaStar } from "react-icons/fa";
 import Button from "antd-button-color";
@@ -33,6 +33,7 @@ function ProductDetail() {
   let [productReview, setProductReview] = useState([]);
   let [listProductNotPage,setListProductNotPage]=useState([]);
   let [textReview, setTextReview] = useState(null);
+  let [messageApi, contextHolder] = message.useMessage();
 
   let paramid = useParams();
   let productid = paramid.productid;
@@ -64,6 +65,7 @@ function ProductDetail() {
         })
         .then((res) => {
           setListProductNotPage(res.data.content);
+          
         })
         .catch((error) => {
           
@@ -143,6 +145,11 @@ let addNewReview=()=>{
   dispatch(actionAddProductReviewAPI(itemProductReview));
   setRatingz("");
   setTextReview("");
+  messageApi.open({
+    type: 'success',
+    content: 'Cảm ơn bạn đã đánh giá cho sản phẩm',
+
+  });
 }
 
  //----------------End Item productReview--------------------
@@ -202,7 +209,7 @@ let addNewReview=()=>{
 
 
   return (
-    <>
+    <>{contextHolder}
       <section className="pt-5 trasua">
         <Container>
           <Row>
--- 3. FrontEnd/src/homepage/Home.jsx
+++ 3. FrontEnd/src/homepage/Home.jsx
@@ -3,8 +3,9 @@ import { Outlet, useNavigate, useLoaderData, Link, } from "react-router-dom";
 import { Layout, Button, Space, Input, Row, Col, Image, Flex } from "antd";
 import Logo from "../assets/Logo.webp";
 import Footers from "./footer";
-import {  ShoppingTwoTone, UserOutlined } from "@ant-design/icons";
+import { ShoppingTwoTone, UserOutlined } from "@ant-design/icons";
 import { RiUserSettingsLine } from "react-icons/ri";
+import { IoIosLogOut } from "react-icons/io";
 // import Cart from "../Pages/Cart";
 
 const { Header } = Layout;
@@ -39,6 +40,15 @@ function Homepage() {
 
 
   }
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("role");
+    localStorage.removeItem("id");
+    navigate('/login');
+
+
+
+  }
 
   const productOnclick = () => {
     navigate('/product');
@@ -60,7 +70,8 @@ function Homepage() {
   const menuProps = {
     items,
   };
-
+  const id = localStorage.getItem("id");
+  const role = localStorage.getItem("role");
 
 
 
@@ -82,25 +93,34 @@ function Homepage() {
             </Col>
             {/* -------------------- Tiến làm-------------------- */}
             <Flex wrap="wrap" gap="small">
-            <h3><Link to={'/admin'}><RiUserSettingsLine /></Link></h3>
-            
-            
-            <h2><Link to={'/cart'}><ShoppingTwoTone /> </Link></h2>
+              {role === 'ADMIN' && (
+                <h3><Link to={'/admin'}><RiUserSettingsLine /></Link></h3>
+              )}
+
+                
+              <h2><Link to={'/cart'}><ShoppingTwoTone /> </Link></h2>
 
             </Flex>
-              
-            
 
-          
+
+
+
             <Col span={2}>
               {/* <Dropdown menu={menuProps}> */}
-              <Button onClick={loginclick} >
-                <Space>
-                  Login
-                  {/* <DownOutlined /> */}
-                </Space>
-              </Button>
+              {id ? null : (
+                <Button onClick={loginclick} >
+                  <Space>
+                    Login
+                    {/* <DownOutlined /> */}
+                  </Space>
+                </Button>
+              )}
               {/* </Dropdown> */}
+            </Col>
+            <Col span={0.1}>
+              {id && (
+                <h2 onClick={logout} style={{ cursor: "pointer" }}><IoIosLogOut /></h2>
+              )}
             </Col>
           </Row>
         </Header>
